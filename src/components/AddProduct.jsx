519543f7a9714aacfea56d68c97011a000afccd1
ishan10902 <ishansugath@gmail.com>
2025-07-14 20:06:18 +0530

    complete

--- src/components/AddProduct.jsx
+++ src/components/AddProduct.jsx
@@ -12,30 +12,27 @@ const AddProduct = () => {
                 <div className="col">
                     <div className="row">
                         <div className="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                            <label htmlFor="" className='form-label'>ID</label>
+                            <label htmlFor="" className='form-label'>TITLE</label>
                             <input type="text" className='form-control' />
                         </div>
                         <div className="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                            <label htmlFor="" className='form-label'>TITLE</label>
+                            <label htmlFor="" className='form-label'>CATEGORY</label>
                             <input type="text" className='form-control' />
                         </div>
                         <div className="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                             <br></br>
                             <label htmlFor="" className='form-label'>DESCRIPTION</label>
-                            <input type="text" className='form-control' />
-                        </div>
-                        <div className="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                            <br></br>
-                            <label htmlFor="" className='form-label'>CATEGORY</label>
                             <textarea name="decription" id="" className='form-control'></textarea>
                         </div>
                         <div className="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <br></br>
                             <label htmlFor="" className='form-label'>PRICE</label>
                             <input type="text" className='form-control' />
                         </div>
                         <div className="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <br></br>
                             <label htmlFor="" className='form-label'>DISCOUNT PERCENTAGE</label>
-                            <input type="file" className='form-control' />
+                            <input type="text" className='form-control' />
                         </div>
                         <div className="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                             <br></br>
